f549e5df21f1bef0170e734e53b52fc4b83ab92c
giacintamrnsa <giacintamaurensa@gmail.com>
2021-08-09 10:53:20 +0700

    initial.commit

---
+++ hello2.r
@@ -0,0 +1,9 @@
+"Hello Selamat Datang di Dunia Pemograman R"
+
+nama = "Giacinta Maurensa"
+umur = 19
+ipk = 3.72
+
+print(nama)
+print(umur)
+print(ipk)
---
+++ titanic.r
@@ -0,0 +1,12 @@
+dfTitanic <- data.frame(Titanic)
+dfTitanic
+
+library(data.table)
+data_titanic = data.table(dfTitanic)
+head(data_titanic)
+
+total_passanger <- data_titanic[, sum(Freq)]
+total_passanger
+
+freq_surv <- data_titanic[, list(n=sum(Freq)), by='Survived']
+freq_surv
